27534fdca88d231fbf8dad9d7d1b33392d5f328d
Darryl <dsalda11@asu.edu>
2025-12-29 20:43:40 -0700

    Initial commit

---
+++ src/main/java/com/sbt/softwarebugtracker/repository/EngineerRepository.java
@@ -0,0 +1,13 @@
+package com.sbt.softwarebugtracker.repository;
+
+import com.sbt.softwarebugtracker.model.Engineer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EngineerRepository extends MongoRepository<Engineer, String> {
+    Optional<Engineer> findEngineerByEmail(String email);
+}
+
